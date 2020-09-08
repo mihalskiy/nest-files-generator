0aef90b6aa814cd871be32f3d45e298abb2a1196
mihalskiy <mihalskiy.rv@gmail.com>
2020-09-08 16:47:24 +0300

    Add precompiled assets for Heroku

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
   app.enableCors();
   app.use(json({ limit: '50mb'}))
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
